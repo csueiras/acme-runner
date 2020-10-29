33f6f41260a422ecbfaa9311e8f536671549bfb9
Christian A. Sueiras <csueiras@gmail.com>
2020-10-29 09:29:34 -0400

    ACME Runner initial release

---
+++ src/main/java/com/github/csueiras/acme/runner/AcmeRunnerApp.java
@@ -0,0 +1,16 @@
+package com.github.csueiras.acme.runner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * App entrypoint
+ *
+ * @author Christian Sueiras
+ */
+@SpringBootApplication
+public class AcmeRunnerApp {
+    public static void main(String[] args) {
+        SpringApplication.run(AcmeRunnerApp.class, args);
+    }
+}
